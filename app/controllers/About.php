2a216367f53261c9451e6686f880d2ff685a55c0
FitraAnanda <fitraanandapku123@gmail.com>
2021-10-01 17:07:39 +0700

    Video ke-5

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,27 @@
 <?php
 
-class About
-{
-    public function index($nama = 'RidhaAF', $pekerjaan = 'Mahasiswa')
-    {
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+<?php 
+
+class About {
+public function index($nama = 'RidhaAF', $pekerjaan = 'Mahasiswa') {
+    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+}
+class About extends Controller {
+    public function index($nama = 'RidhaAF', $pekerjaan = 'Mahasiswa', $umur = 19) {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
-    public function page()
-    {
+    public function page() {
         echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
